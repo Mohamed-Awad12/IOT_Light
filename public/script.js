d6373c3e8a39f4160a77344e9dfde35665fd1df4
MohamedAwad <Mohamed.Awad@dmu.edu.eg>
2025-12-19 14:03:08 +0200

    update

--- public/script.js
+++ public/script.js
@@ -203,10 +203,10 @@ document.addEventListener('touchmove', (e) => {
     pullCord.style.transform = `translateY(${currentY}px)`;
     
     if (currentY > pullThreshold) {
-        pullHint.textContent = 'Release to toggle!';
+      
         pullHint.classList.add('visible');
     } else {
-        pullHint.textContent = 'Pull to toggle';
+      
         pullHint.classList.remove('visible');
     }
 });
@@ -222,7 +222,7 @@ document.addEventListener('touchend', () => {
     pullCord.style.transform = '';
     
     pullHint.classList.remove('visible');
-    pullHint.textContent = 'Pull to toggle';
+    
     
     if (wasPulledEnough) {
         toggleLampWithCord();
